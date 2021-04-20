f354ccb4178ba1f9731200b8113125aefa596ac2
Miles Debinski <miles.debinski@gmail.com>
2021-04-20 11:38:00 +0100

    add new select list

--- script.js
+++ script.js
@@ -3,89 +3,94 @@ const api_RestCountries = `https://restcountries.eu/rest/v2/currency/`;
 const api_Fixer = {
   key: "04fb41552e9d5d990f164335b5d231e5",
   base: "http://data.fixer.io/api/"
-  // latest?access_key=
+
 }
 
 
-//global variables
-const hide = document.querySelector('.hide')
+//Global variables
+let fixerAPIdata;
 const convert = document.querySelector('.convert')
-const fromCurrency = document.querySelector('.fromCurrency')
-const toCurrency = document.querySelector('.toCurrency')
-const resultFrom = document.querySelector('.resultFrom')
-const resultTo = document.querySelector('.resultTo')
-const howMuch = document.querySelector('.howMuch')
-const rate = document.querySelector('.rate')
 const countries = document.querySelector('.countries')
-const country = document.querySelector('.country')
-convert.addEventListener('click', getCurrencyAPI);
+convert.addEventListener('click', fromCurrencyTo);
 
-// 1. Fetch data about currencies
-function getCurrencyAPI() {
-  hide.style.display = 'unset';
+//1. Get first list
+function getList() {
+  const optionsFrom = document.querySelector('.optionsFrom')
   fetch(`${api_Fixer.base}latest?access_key=${api_Fixer.key}`)
     .then(fixerAPI => {
       return fixerAPI.json();
-    }).then(fromCurrencyTo)
+    }).then(fixerAPI => {
+      fixerAPIdata = fixerAPI;
+      for (const [key] of Object.entries(fixerAPI.rates)) {
+        let newOption = document.createElement('option');
+        newOption.innerText = key;
+        optionsFrom.appendChild(newOption);
+        optionsFrom.value = 'GBP'
+      }
+    }).then(getSecondList);
 }
 
+//2. Get second list
+function getSecondList() {
+  const optionsTo = document.querySelector('.optionsTo')
+  fetch(`${api_Fixer.base}latest?access_key=${api_Fixer.key}`)
+    .then(fixerAPI => {
+      return fixerAPI.json();
+    }).then(fixerAPI => {
+      for (const [key] of Object.entries(fixerAPI.rates)) {
+        let newOption = document.createElement('option');
+        newOption.innerText = key;
+        optionsTo.appendChild(newOption);
+        optionsTo.value = 'USD'
+      }
+    })
+}
+getList();
 
-// 2. Convert & Display currencies
-function fromCurrencyTo(fAPI) {
+// 3. Convert & Display currencies
+function fromCurrencyTo() {
+  // Variables
+  const fromCurrency = document.querySelector('.fromCurrency')
+  const toCurrency = document.querySelector('.toCurrency')
+  const resultFrom = document.querySelector('.resultFrom')
+  const resultTo = document.querySelector('.resultTo')
+  const hide = document.querySelector('.hide')
+  const howMuch = document.querySelector('.howMuch')
+  const rate = document.querySelector('.rate')
+
+  hide.style.display = 'unset';
   fromCurrency.value = fromCurrency.value.toUpperCase();
   toCurrency.value = toCurrency.value.toUpperCase();
 
-
-  const euro = 1 / fAPI.rates[fromCurrency.value]
-  const exchangeRate = euro * fAPI.rates[toCurrency.value]
+  // ExchangeRate & Conversion
+  const euro = 1 / fixerAPIdata.rates[fromCurrency.value]
+  const exchangeRate = euro * fixerAPIdata.rates[toCurrency.value]
   const convertedAmount = (howMuch.value * exchangeRate).toFixed(2);
   rate.innerText = exchangeRate.toFixed(2);
   resultFrom.innerText = `${howMuch.value} ${fromCurrency.value}`;
   resultTo.innerText = `${convertedAmount} ${toCurrency.value}`;
 
-
-
   // Clear the list of countries.
   while (countries.lastElementChild) {
     countries.removeChild(countries.lastElementChild);
   }
-  // interesting: https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
-
-  // HOW TO MAKE A LIST INSTEAD OF AN INPUT USING API?
   getCountriesAPI(toCurrency.value);
 }
 
-// 3. Fetch data about countries
+// 4. Fetch data about countries
 function getCountriesAPI(currencyCode) {
-  // https://restcountries.eu/rest/v2/name/{name}
   fetch(`${api_RestCountries}${currencyCode}`)
     .then(countriesAPI => {
       return countriesAPI.json();
     }).then(displayCountries)
 }
 
-
-
-// 4. Fetch data about cost of living 
-
-
-
-
 // 5. Display data about countries
-
 function displayCountries(cAPI) {
   for (let i = 0; i < cAPI.length; i++) {
     let newCountry = document.createElement('li');
     newCountry.setAttribute('class', 'country');
     newCountry.innerText = `${cAPI[i].name}`;
     countries.appendChild(newCountry);
-
   }
-
-  console.log(cAPI)
-
-
-
-
-
 }
