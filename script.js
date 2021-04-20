37894ba4871fb031113a2f2ed3305be8f1639733
Miles Debinski <miles.debinski@gmail.com>
2021-04-20 17:31:36 +0100

    input only positive numbers

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const api_RestCountries = `https://restcountries.eu/rest/v2/currency/`;
 const api_Fixer = {
   key: "04fb41552e9d5d990f164335b5d231e5",
   base: "http://data.fixer.io/api/"
-
 }
 
 // Global variables
